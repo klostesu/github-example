35de781d88e84b23de40bca0e9c2d58d50bc1b0c
klost <klost@SUESHPLAPTOP>
2025-04-02 23:25:47 -0400

    Add project files.

---
+++ github example.cpp
@@ -0,0 +1,13 @@
+// github example.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Sue Klosterman
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
